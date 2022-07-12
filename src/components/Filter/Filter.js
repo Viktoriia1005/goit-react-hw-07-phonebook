fc3a0cb18422536fcc8a8d83f88b12b006570f2d
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-07-12 18:11:44 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { getFilter } from '../redux/selectors';
-
+import { changeFilter } from '../redux/actions';
 import s from './Filter.module.css';
 
 export default function Filter() {
   const filterValue = useSelector(getFilter);
   const dispatch = useDispatch();
 
-  const changeFilter = e => dispatch(changeFilter(e.target.value));
+  const change = e => dispatch(changeFilter(e.target.value));
 
   return (
     <label className={s.label}>
@@ -17,7 +17,7 @@ export default function Filter() {
         type="text"
         name="name"
         value={filterValue}
-        onChange={changeFilter}
+        onChange={change}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
